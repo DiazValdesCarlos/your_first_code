2d26e347a97ea5cac8926fc2b101bf90ec6e5eb9
DiazValdesCarlos <143344432+DiazValdesCarlos@users.noreply.github.com>
2023-08-27 12:49:58 -0400

    Hello School

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
-}
+School
+School
+School
+School
+School
+School
+School
+School
+School
+School
